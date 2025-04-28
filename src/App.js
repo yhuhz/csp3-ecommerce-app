0b4a4d1c1fd66e6b80e37c628ddd3d32194eab92
julius.albert.grajo <julius.albert.grajo@gmail.com>
2025-04-28 09:19:56 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import AppNavbar from './components/AppNavbar';
+import Home from './pages/Home';
 
 function App() {
+  const [user, setUser] = useState({
+    id: null,
+    isAdmin: null,
+  });
+
+  function unsetUser() {
+    localStorage.clear();
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('token') !== null) {
+      fetch('http://localhost:4000/users/details', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUser({
+            id: data._id,
+            isAdmin: data.isAdmin,
+          });
+        });
+    } else {
+      setUser({
+        id: null,
+        isAdmin: null,
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider value={{ user, setUser, unsetUser }}>
+      <Router>
+        <AppNavbar />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </Router>
+    </UserProvider>
   );
 }
 
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+import { useState, useContext } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, NavLink } from 'react-router-dom';
+import UserContext from '../context/UserContext';
+
+export default function AppNavbar() {
+  // const [user, setUser] = useState(localStorage.getItem('token'));
+  const { user } = useContext(UserContext);
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          E-Commerce App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            {user.id !== null ? (
+              <>
+                <Nav.Link as={NavLink} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/logout">
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={NavLink} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
