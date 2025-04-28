1d63941f99684cd8038953b34849df83b3b94fae
[Nacel John Sotomayor] <[naceljohnsotomayor@gmail.com]>
2025-04-28 10:56:34 +0800

    added products page

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,5 +1,29 @@
 import { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-export default function Products() {
-  return <></>;
+
+
+export default function UserView({ productData }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(
+      productData.map((products) => {
+        return <></>
+      })
+    );
+  }, [productData]);
+
+  return (
+    <>
+      <Row className="justify-content-center mt-5">
+        <Col md={8} className="text-center">
+          <h1 className="mb-4">Our Products</h1>
+        </Col>
+      </Row>
+      
+
+      {products}
+    </>
+  );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { UserProvider } from '../context/UserContext';
+import UserView from '../components/UserView';
+import AdminView from '../components/AdminView';
 
 export default function Products() {
-  return <></>;
+  const {user} = useContext(UserProvider);
+  const [productData, setProductData] = useState([]);
+
+  const fetchData = () => {
+    let fetchUrl =
+      user.isAdmin === true
+        ? 'http://localhost:4000/products/all'
+        : 'http://localhost:4000/products/active';
+
+    fetch(fetchUrl, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProductData(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [user]);
+
+  return (
+    <>
+      {user.isAdmin ? (
+        <AdminView productData={productData} fetchData={fetchData} />
+      ) : (
+        <UserView productData={productData} />
+      )}
+    </>
+  );
 }
