5e4feb58185de2d018240e7f38e71c06fe7948a3
julius.albert.grajo <julius.albert.grajo@gmail.com>
2025-04-29 16:43:03 +0800

    Workaround localStorage instead of {user}

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,10 +1,11 @@
 import { useState, useEffect, useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
 import UserContext from '../context/UserContext';
 
 export default function Orders() {
-  const { user } = useContext(UserContext);
+  const user = localStorage.getItem('token');
+
   const [orderData, setOrderData] = useState([]);
 
   const fetchData = () => {
@@ -17,93 +18,97 @@ export default function Orders() {
     })
       .then((res) => res.json())
       .then((data) => {
-        const rows = [];
+        if (data.orders.length > 0) {
+          const rows = [];
 
-        data.orders.forEach((order) => {
-          let orderArray = [];
+          data.orders.forEach((order) => {
+            let orderArray = [];
 
-          if (user.isAdmin) {
-            order.productsOrdered.forEach((product) => {
-              orderArray.push({
-                orderId: order._id,
-                productId: product.productId,
-                productName: product.productName,
-                quantity: product.quantity,
-                subtotal: product.subtotal,
-                orderedOn: order.orderedOn,
+            if (user.isAdmin) {
+              order.productsOrdered.forEach((product) => {
+                orderArray.push({
+                  orderId: order._id,
+                  productId: product.productId,
+                  productName: product.productName,
+                  quantity: product.quantity,
+                  subtotal: product.subtotal,
+                  orderedOn: order.orderedOn,
+                });
               });
-            });
 
-            orderArray.forEach((o) => {
-              rows.push(
-                <tr key={`${o.productId}-${o.orderedOn}`}>
-                  <td>{o.orderId}</td>
-                  <td>{o.productName}</td>
-                  <td>
-                    {new Intl.NumberFormat('en-PH', {
-                      style: 'currency',
-                      currency: 'PHP',
-                    }).format(o.subtotal / o.quantity)}
-                  </td>
-                  <td>{o.quantity}</td>
-                  <td>
-                    {new Intl.NumberFormat('en-PH', {
-                      style: 'currency',
-                      currency: 'PHP',
-                    }).format(o.subtotal)}
-                  </td>
-                  <td>
-                    {new Date(o.orderedOn).toLocaleDateString()}{' '}
-                    {new Date(o.orderedOn).toLocaleTimeString([], {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </td>
-                </tr>
-              );
-            });
-          } else {
-            order.productsOrdered.forEach((product) => {
-              orderArray.push({
-                productId: product.productId,
-                productName: product.productName,
-                quantity: product.quantity,
-                subtotal: product.subtotal,
-                orderedOn: order.orderedOn,
+              orderArray.forEach((o) => {
+                rows.push(
+                  <tr key={`${o.productId}-${o.orderedOn}`}>
+                    <td>{o.orderId}</td>
+                    <td>{o.productName}</td>
+                    <td>
+                      {new Intl.NumberFormat('en-PH', {
+                        style: 'currency',
+                        currency: 'PHP',
+                      }).format(o.subtotal / o.quantity)}
+                    </td>
+                    <td>{o.quantity}</td>
+                    <td>
+                      {new Intl.NumberFormat('en-PH', {
+                        style: 'currency',
+                        currency: 'PHP',
+                      }).format(o.subtotal)}
+                    </td>
+                    <td>
+                      {new Date(o.orderedOn).toLocaleDateString()}{' '}
+                      {new Date(o.orderedOn).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </td>
+                  </tr>
+                );
               });
-            });
+            } else {
+              order.productsOrdered.forEach((product) => {
+                orderArray.push({
+                  productId: product.productId,
+                  productName: product.productName,
+                  quantity: product.quantity,
+                  subtotal: product.subtotal,
+                  orderedOn: order.orderedOn,
+                });
+              });
 
-            orderArray.forEach((o) => {
-              rows.push(
-                <tr key={`${o.productId}-${o.orderedOn}`}>
-                  <td>{o.productName}</td>
-                  <td>
-                    {new Intl.NumberFormat('en-PH', {
-                      style: 'currency',
-                      currency: 'PHP',
-                    }).format(o.subtotal / o.quantity)}
-                  </td>
-                  <td>{o.quantity}</td>
-                  <td>
-                    {new Intl.NumberFormat('en-PH', {
-                      style: 'currency',
-                      currency: 'PHP',
-                    }).format(o.subtotal)}
-                  </td>
-                  <td>
-                    {new Date(o.orderedOn).toLocaleDateString()}{' '}
-                    {new Date(o.orderedOn).toLocaleTimeString([], {
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </td>
-                </tr>
-              );
-            });
-          }
-        });
+              orderArray.forEach((o) => {
+                rows.push(
+                  <tr key={`${o.productId}-${o.orderedOn}`}>
+                    <td>{o.productName}</td>
+                    <td>
+                      {new Intl.NumberFormat('en-PH', {
+                        style: 'currency',
+                        currency: 'PHP',
+                      }).format(o.subtotal / o.quantity)}
+                    </td>
+                    <td>{o.quantity}</td>
+                    <td>
+                      {new Intl.NumberFormat('en-PH', {
+                        style: 'currency',
+                        currency: 'PHP',
+                      }).format(o.subtotal)}
+                    </td>
+                    <td>
+                      {new Date(o.orderedOn).toLocaleDateString()}{' '}
+                      {new Date(o.orderedOn).toLocaleTimeString([], {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </td>
+                  </tr>
+                );
+              });
+            }
+          });
 
-        setOrderData(rows);
+          setOrderData(rows);
+        } else {
+          setOrderData([]);
+        }
       });
   };
 
@@ -111,44 +116,54 @@ export default function Orders() {
     fetchData();
   }, [user]);
 
-  return user.id === null ? (
-    <Navigate to={'/'} />
-  ) : (
+  return user && user.id !== null ? (
     <>
       {user.isAdmin ? (
         <>
-          <h3>All Orders</h3>
-          <Table striped bordered hover responsive size="sm">
-            <thead>
-              <tr>
-                <th>Order ID</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-                <th>Order Date</th>
-              </tr>
-            </thead>
-            <tbody>{orderData}</tbody>
-          </Table>
+          <h3 className="text-center my-5">All Orders</h3>
+          {orderData.length > 0 ? (
+            <Table striped bordered hover responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Order ID</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Subtotal</th>
+                  <th>Order Date</th>
+                </tr>
+              </thead>
+              <tbody>{orderData}</tbody>
+            </Table>
+          ) : (
+            <h5 className="text-center">No orders yet.</h5>
+          )}
         </>
       ) : (
         <>
           <h3 className="text-center my-5">My Orders</h3>
-          <Table striped bordered hover responsive size="sm">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-                <th>Order Date</th>
-              </tr>
-            </thead>
-            <tbody>{orderData}</tbody>
-          </Table>
+          {orderData.length > 0 ? (
+            <Table striped bordered hover responsive size="sm">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Subtotal</th>
+                  <th>Order Date</th>
+                </tr>
+              </thead>
+              <tbody>{orderData}</tbody>
+            </Table>
+          ) : (
+            <h5 className="text-center">
+              You have no orders yet. <Link to={'/products'}>Shop now!</Link>{' '}
+            </h5>
+          )}
         </>
       )}
     </>
+  ) : (
+    <Navigate to={'/'} />
   );
 }
