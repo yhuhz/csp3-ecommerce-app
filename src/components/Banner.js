6c0e77392b2cf14b2b633a003efcd832f4d769ab
Julius Grajo <julius.albert.grajo.com>
2025-04-30 10:08:40 +0800

    Added quantity option in product view

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,7 +20,7 @@ const Banner = () => {
                 borderRadius: '8px',
               }}
             >
-              <h1>Welcome to Our Store!</h1>
+              <h1>Welcome to JN Store!</h1>
               <p>
                 Your one-stop shop for everything you love. Explore now and find
                 amazing deals!
--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -14,6 +14,7 @@ export default function ProductView() {
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState(0);
   const [image, setImage] = useState('');
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_BASE_URL}/products/${productId}`)
@@ -26,7 +27,8 @@ export default function ProductView() {
       });
   }, [user]);
 
-  const addToCart = () => {
+  const addToCart = (e) => {
+    e.preventDefault();
     fetch(`${process.env.REACT_APP_API_BASE_URL}/cart/add-to-cart`, {
       method: 'POST',
       headers: {
@@ -35,7 +37,7 @@ export default function ProductView() {
       },
       body: JSON.stringify({
         productId: productId,
-        quantity: 1,
+        quantity: quantity,
       }),
     })
       .then((res) => res.json())
@@ -83,18 +85,54 @@ export default function ProductView() {
 
               {user.id !== null ? (
                 !user.isAdmin ? (
-                  <Button
-                    variant="primary"
-                    className="w-100 mt-3"
-                    onClick={addToCart}
-                  >
-                    Add to Cart
-                  </Button>
+                  <>
+                    <div className="d-flex flex-column align-items-center gap-2">
+                      <span>Quantity:</span>
+                      <div className="d-flex justify-content-center gap-2">
+                        <Button
+                          variant="outline-secondary"
+                          onClick={() => {
+                            if (quantity === 1 || quantity < 1) {
+                              notyf.error('Quantity cannot be less than one');
+                            } else {
+                              setQuantity(quantity - 1);
+                            }
+                          }}
+                        >
+                          <i className="bi bi-dash"></i>
+                        </Button>
+
+                        <input
+                          type="number"
+                          className="form-control text-center px-0"
+                          value={quantity}
+                          onChange={(e) => setQuantity(Number(e.target.value))}
+                          style={{ width: '60px' }} // Adjust width if needed
+                        />
+
+                        <Button
+                          variant="outline-secondary"
+                          onClick={() => setQuantity(quantity + 1)}
+                        >
+                          <i className="bi bi-plus"></i>
+                        </Button>
+                      </div>
+                    </div>
+
+                    <Button
+                      variant="success"
+                      className="w-100 mt-3"
+                      onClick={addToCart}
+                      disabled={quantity < 1}
+                    >
+                      Add to Cart
+                    </Button>
+                  </>
                 ) : (
                   ''
                 )
               ) : (
-                <Link to="/login" className="btn btn-danger w-100 mt-3">
+                <Link to="/login" className="btn btn-warning w-100 mt-3">
                   Login to Add to Cart
                 </Link>
               )}
